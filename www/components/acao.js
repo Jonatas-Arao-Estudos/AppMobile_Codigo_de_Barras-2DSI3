233f5067e3c718147186f6afcffef54eaa6cd253
Jonatas-Arao-Estudos <jonatas12apostolico@hotmail.com>
2019-08-11 10:02:36 +0900

    Iniciando os Cards

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,10 +3,7 @@
 $(document).on("click","#scan",function(){
   cordova.plugins.barcodeScanner.scan(
         function (result) {
-            alert("We got a barcode\n" +
-                  "Result: " + result.text + "\n" +
-                  "Format: " + result.format + "\n" +
-                  "Cancelled: " + result.cancelled);
+          alert("Scaneado");
         },
         function (error) {
             alert("Scanning failed: " + error);
